2445e87765c823ce90da51ef40ec20a06d51998e
Paulo Carneiro <paulo.acbb@hotmail.com>
2022-04-26 18:34:54 -0300

    Add basic ui

--- myscript.js
+++ myscript.js
@@ -1,45 +1,96 @@
-    /*Usuario Escolher pedra papel tesoura
-    Apenas aceitar uma das tres
-    Computador Escolher pedra papel tesoura aletoriamente
-    Comparar escolha de usario com de computador
-    */
-    let pontosUsario = 0;
-    let pontosComp = 0;
+/*Usuario Escolher pedra papel tesoura
+Computador Escolher pedra papel tesoura aletoriamente
+Comparar escolha de usario com de computador
+*/
+let pontosUsario = 0;
+let pontosComp = 0;
+const resultsDiv = document.querySelector('#resultsDiv');
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
 
-    function escolhaDoUser() {
-      let escolhaUser = prompt("Pedra, Papel, Tesoura", "Escolha um");
-      console.log("Escolha do Usuario: " + escolhaUser);
-      let escolha = escolhaUser.toLocaleLowerCase()
-      if (escolha !== "pedra" && escolha !== "papel" && escolha !== "tesoura") {
-        alert("Erro");
-      }
-      return escolha;
-    }
-    function computerPlay() {
-      let aleatorio = ["pedra", "papel", "tesoura"];
-      return aleatorio[Math.floor(Math.random() * 3)];
-    }
-    function playRound(escolha, escolhaComp) {
-      if (escolha === escolhaComp) {
-        alert("Empate");
-      }
-      else if (escolha == "pedra" && escolhaComp == "tesoura" || escolha == "papel" && escolhaComp == "pedra" ||
-        escolha == "tesoura" && escolhaComp == "papel") {
-        alert("Ganhou"); pontosUsario++;
-      }
-      else if (escolha == "pedra" && escolhaComp == "papel" || escolha == "papel" && escolhaComp == "tesoura" ||
-        escolha == "tesoura" && escolhaComp == "pedra") {
-        alert("Perdeu"); pontosComp++;
-      }
-    }
-    function game() {
-      for (let i = 0; i < 5; i++) {
-      let escolhaDoUsuario = escolhaDoUser();
-      let escolhaComp = computerPlay();
-      console.log("Escolha do Computador: " + escolhaComp);
-      playRound(escolhaDoUsuario, escolhaComp);
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    let playerSelection = button.id;
+    console.log(`Escolha do Usuario: ${playerSelection}`);
+    let escolhaComp = computerPlay();
+    console.log(`Escolha do Comp: ${escolhaComp}`)
+    playRound(playerSelection, escolhaComp);
+  });
+});
+
+function computerPlay() {
+  let aleatorio = ["pedra", "papel", "tesoura"];
+  return aleatorio[Math.floor(Math.random() * 3)];
 }
-      console.log("Pontos do Usario ", pontosUsario);
-      console.log("Pontos do Computador ", pontosComp);
-    }
-    game();
+function playRound(escolha, escolhaComp) {
+  if(pontosUsario == 5){
+    const newP = document.createElement("p");
+    const pTxt = document.createTextNode(`GANHOU A PARTIDA Pontos do Usuario:${pontosUsario} Pontos do Computador:${pontosComp}`)
+    newP.style.color ="blue"
+    newP.appendChild(pTxt);
+    resultsDiv.appendChild(newP);
+    return;
+  }
+  
+  else if(pontosComp == 5){
+    const newP = document.createElement("p");
+    const pTxt = document.createTextNode(`PERDEU A PARTIDA Pontos do Usuario:${pontosUsario} Pontos do Computador:${pontosComp}`)
+    newP.style.color ="purple"
+    newP.appendChild(pTxt);
+    resultsDiv.appendChild(newP);
+    return;
+  }
+
+  if (escolha === escolhaComp) {
+    const newP = document.createElement("p");
+    const pTxt = document.createTextNode(`Empate Pontos do Usuario:${pontosUsario} Pontos do Computador:${pontosComp}`)
+    newP.appendChild(pTxt);
+    resultsDiv.appendChild(newP);
+  }
+  else if (escolha == "pedra" && escolhaComp == "tesoura" || escolha == "papel" && escolhaComp == "pedra" ||
+    escolha == "tesoura" && escolhaComp == "papel") {
+    pontosUsario++;  
+    const newP = document.createElement("p");
+    const pTxt = document.createTextNode(`Ganhou Pontos do Usuario:${pontosUsario} Pontos do Computador:${pontosComp}`)
+    newP.style.color ="green"
+    newP.appendChild(pTxt);
+    resultsDiv.appendChild(newP);
+    
+  }
+  else if (escolha == "pedra" && escolhaComp == "papel" || escolha == "papel" && escolhaComp == "tesoura" ||
+    escolha == "tesoura" && escolhaComp == "pedra") {
+    pontosComp++; 
+    const newP = document.createElement("p");
+    const pTxt = document.createTextNode(`Perdeu Pontos do Usuario:${pontosUsario} Pontos do Computador:${pontosComp}`)
+    newP.style.color ="red"
+    newP.appendChild(pTxt);
+    resultsDiv.appendChild(newP);
+    
+  } 
+    console.log(pontosUsario);
+    console.log(pontosComp); 
+
+}
+
+function checkWinner(pontosUsario, pontosComp){
+  if(pontosUsario == 5){
+    const newP = document.createElement("p");
+    const pTxt = document.createTextNode(`GANHOU A PARTIDA Pontos do Usuario:${pontosUsario} Pontos do Computador:${pontosComp}`)
+    newP.style.color ="blue"
+    newP.appendChild(pTxt);
+    resultsDiv.appendChild(newP);
+  }
+  
+  else if(pontosComp == 5){
+    const newP = document.createElement("p");
+    const pTxt = document.createTextNode(`PERDEU A PARTIDA Pontos do Usuario:${pontosUsario} Pontos do Computador:${pontosComp}`)
+    newP.style.color ="purple"
+    newP.appendChild(pTxt);
+    resultsDiv.appendChild(newP);
+  }
+  return
+}
+
